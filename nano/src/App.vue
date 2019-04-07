00207ee8622cc717e4d0fcadb3e515a1fde8215c
Kang <kangkrkr@naver.com>
2019-04-07 17:00:33 +0900

    컴포넌트간 통신예제 작성

--- nano/src/App.vue
+++ nano/src/App.vue
@@ -1,12 +1,55 @@
 <template>
   <div id="app">
+    <!-- 자식컴포넌트의 'headerMsg' Prop은 부모컴포넌트인 App의 -->
+    <!-- data의 Header에 바인딩된다. -->
+    <CommonHeader v-bind:headerMsg="Header" v-on:childClicked="onChildClicked"></CommonHeader>
+
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
     <router-view />
+
+    <!-- 부모컴포넌트가 자식컴포넌트의 props data 정의 -->
+    <CommonFooter footerMsg='Footer'></CommonFooter>
   </div>
 </template>
+
+<script>
+import CommonHeader from "@/components/commons/CommonHeader.vue";
+import CommonFooter from "@/components/commons/CommonFooter.vue";
+
+
+export default {
+  name : 'App',
+  components : {
+    CommonHeader,
+    CommonFooter,
+  },
+  data : function() {
+      return {
+          Header : 'This is Header!',
+          alertMsg : 'The App Component was created!',
+      }
+  },
+  created : function() {
+    // this는 해당 컴포넌트를 의미
+    this.popAlert(this.alertMsg);
+  },
+  computed : {
+    
+  },
+  methods : {
+    popAlert : function(msg) {
+      alert(msg);
+    },
+    onChildClicked : function(evt) {
+      this.Header = 'child was clicked!';
+    }
+  }
+}
+</script>
+
 
 <style>
 #app {
---
+++ nano/src/components/commons/CommonHeader.vue
@@ -0,0 +1,29 @@
+<template>
+    <!-- 자식컴포넌트인 CommonHeader가 부모컴포넌트 App의 데이터를 바꾸려면, -->
+    <!-- 이벤트 버스를 통해 데이터를 변경하여야 한다. -->
+    <div id="common-header" v-on:click="clickMe">{{headerMsg}}</div>
+</template>
+
+<script>
+    export default {
+        name : 'CommonHeader',
+        // 자식컴포넌트는, props를 정의함으로써
+        // 부모컴포넌트가 자식컴포넌트 데이터를 변경할 수 있도록 한다.
+        props : {
+            headerMsg : String,
+            content : String,
+        },
+        methods : {
+            clickMe : function() {
+                this.$emit('childClicked');
+            },
+        }
+        
+    };
+</script>
+
+<style>
+div#common-header {
+    border: 1px solid gray;
+}
+</style>

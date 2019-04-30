532262e76638870e64c0b4b6b71d45fd529dc8f2
Kang <kangkrkr@naver.com>
2019-05-01 00:18:05 +0900

    ...

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const serveStatic = require("serve-static")
 const path = require('path');
+
+console.log(__dirname);
+
 app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
 const port = process.env.PORT || 5000;

02bd75df36bad0343ba27f9043fd790810e6f2e5
Kang <kangkrkr@naver.com>
2019-05-01 01:15:37 +0900

    express rewriting rule 지정

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const serveStatic = require("serve-static")
-const path = require('path');
+const express      = require('express');
+const serveStatic  = require("serve-static")
+const path         = require('path');
 
 console.log(__dirname);
 
@@ -8,3 +8,8 @@ app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
 const port = process.env.PORT || 5000;
 app.listen(port);
+
+app.route('/*').get(function(req, res) {
+    console.log(req);
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
+});

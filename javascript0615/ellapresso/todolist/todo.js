d3832d4a63fbabfb3c45b836b7aaca0a34186198
ellapresso <ellapresso@gmail.com>
2019-07-03 13:11:28 +0900

    중간저장

--- javascript0615/ellapresso/todolist/todo.js
+++ javascript0615/ellapresso/todolist/todo.js
@@ -7,7 +7,6 @@ const localList = "localList";
 
 //input에서 가져온 값 저장하기
 function saveToDo(form) {
-    loadToDo(); //저장전 기존 데이터 불러오기
     let num = toDoList.length + 1
     const toDo = {
         num,
@@ -38,15 +37,14 @@ function loadToDo() {
 function savedlist(text) {
     let num = toDoList.length + 1
     const li = document.createElement("li");
-    li.id = num;
-
+    li.id = "todo" + num;
+    li.className = "collection-item"
     li.innerHTML = `<label>
 <input type="checkbox" />
-<span> 1st TO DO</span>
+<span>` + text + `</span>
 <input type="button" class="btn-small purple darken-4" value="del" />
 <input type="button" class="btn-small purple darken-4" value="edit" />
 </label>`
-    console.log(li.innerHTML);
     listBox.appendChild(li);
     const toDo = {
         num,
@@ -55,14 +53,4 @@ function savedlist(text) {
     toDoList.push(toDo);
 }
 
-function handleSubmit(event) {
-    event.preventDefault();
-    const currentValue = inputToDo.value;
-    paintToDo(currentValue);
-    inputToDo.value = "";
-}
-
-function init() {
-    loadToDos();
-    formToDo.addEventListener("submit", handleSubmit);
-}
+loadToDo();

1f90ddba767f103de05c7d15fb7ea9ae6f276a23
ellapresso <ellapresso@gmail.com>
2019-07-04 15:05:16 +0900

    수정완료

--- javascript0615/ellapresso/todolist/todo.js
+++ javascript0615/ellapresso/todolist/todo.js
@@ -1,13 +1,13 @@
-const formToDo = document.querySelector(".formTodo"),
-    inputToDo = document.querySelector(".inputTodo"),
-    listBox = document.querySelector(".listBox");
+const formToDo = document.querySelector('.formTodo'),
+    inputToDo = document.querySelector('.inputTodo'),
+    listBox = document.querySelector('.listBox');
 
 let toDoList = [];
-const localList = "localList";
+const localList = 'localList';
 
 //input에서 가져온 값 저장하기
 function saveToDo(form) {
-    let num = toDoList.length + 1
+    let num = toDoList.length + 1;
     const toDo = {
         num,
         contents: form.inputTD.value
@@ -21,7 +21,7 @@ function saveLocal() {
     localStorage.setItem(localList, JSON.stringify(toDoList));
 }
 
-//기존데이터 불러와서 
+//기존데이터 불러와서
 function loadToDo() {
     const localData = localStorage.getItem(localList);
     if (localData != null) {
@@ -35,16 +35,19 @@ function loadToDo() {
 
 //기존 데이터 리스트에 저장
 function savedlist(text) {
-    let num = toDoList.length + 1
-    const li = document.createElement("li");
+    let num = toDoList.length + 1;
+    const li = document.createElement('li');
     li.id = num;
-    li.className = "collection-item"
-    li.innerHTML = `<label>
+    li.className = 'collection-item';
+    li.innerHTML =
+        `<label>
 <input type="checkbox" />
-<span>` + text + `</span>
+<span>` +
+        text +
+        `</span>
 <input type="button" class="btn-small purple darken-4" onClick="deleteList(this)" value="del" />
 <input type="button" class="btn-small purple darken-4" onClick="editList(this)" value="edit" />
-</label>`
+</label>`;
     listBox.appendChild(li);
     const toDo = {
         num,
@@ -63,5 +66,20 @@ function deleteList(v) {
     saveLocal();
 }
 
-function editList(v) {}
+function editList(v) {
+    const id = v.parentNode.parentNode.id;
+    console.log(document.getElementById(id).innerHTML)
+    document.getElementById(id).innerHTML = `<form id="` + id + `"onsubmit="return editToDo(this);">
+<div class="input-field col s10">
+<input class="inputTodo" name="inputTD" type="text" value="` + toDoList[id - 1].contents + `"placeholder="INPUT TO DO" />
+</div></form>
+`
+}
+
+function editToDo(form) {
+    const id = form.id - 1;
+    toDoList[id].contents = form.inputTD.value;
+    saveLocal()
+}
+
 loadToDo();
